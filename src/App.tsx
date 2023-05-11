1fc26c7b524432cb89a140939f498863c5eaf9e8
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-11 12:15:20 +0200

    base stucture done

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+
+    return (
+        <div>
+            <div>
+                picker1
+                <div>icon</div>
+                <div>title</div>
+                <div>message</div>
+            </div>
+            <div>
+                picker2
+                <div>icon</div>
+                <div>title</div>
+                <div>message</div>
+            </div>
+            <div>
+                picker3
+                <div>icon</div>
+                <div>title</div>
+                <div>message</div>
+            </div>
+        </div>
+    )
+}
+
+export default App
