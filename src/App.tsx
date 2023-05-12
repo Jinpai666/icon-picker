59f8cf479fe02dfcd074c0dce910530242622b85
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-12 15:38:35 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,14 @@ const App = () => {
     return (
         <div>
            <PickerColumn
-               icon="icon1"
                 title="title1"
                 message="message1"
            />
             <PickerColumn
-                icon="icon2"
                 title="title2"
                 message="message2"
             />
             <PickerColumn
-                icon="icon3"
                 title="title3"
                 message="message3"
             />
