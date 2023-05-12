40429e40af5202e69daf7b9610fdae5badcd8481
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-12 15:36:14 +0200

    add save to pdf logic

--- src/components/PickerColumn.tsx
+++ src/components/PickerColumn.tsx
@@ -1,23 +1,60 @@
-import {FC, useState} from 'react';
-import Modal from "./PickerModal";
+import React, { FC, useState, RefObject } from 'react';
+import Modal from './PickerModal';
+import html2pdf, { Options } from 'html2pdf.js';
+
+import { AccountCircle } from '@mui/icons-material';
 
 interface PickerProps {
-    icon: string;
     title: string;
     message: string;
 }
 
-const PickerColumn: FC<PickerProps> = ({ message,icon,title }) => {
+const PickerColumn: FC<PickerProps> = ({ message, title }) => {
     const [showModal, setShowModal] = useState<boolean>(false);
-    const handleIconClick = (): void => setShowModal(!showModal);
+    const pickerColumnRef: RefObject<HTMLDivElement> = React.createRef();
+    const buttonRef: RefObject<HTMLButtonElement> = React.createRef();
+
+    const handleIconClick = () => setShowModal(!showModal);
+
+    const save = () => {
+        const element = pickerColumnRef.current;
+        const button = buttonRef.current;
+        const options: Options = {
+            filename: 'slide.pdf',
+            margin: 10,
+            jsPDF: { format: 'a4', orientation: 'portrait' },
+        };
+
+        if (button) {
+            button.style.display = 'none';
+        }
+
+        html2pdf()
+            .set(options)
+            .from(element)
+            .save()
+            .then(() => {
+                if (button) {
+                    button.style.display = 'block'; // Show after PDF generation
+                }
+            });
+    };
+
     return (
-        <div>
-            <div onClick={handleIconClick}>{icon}</div>
+        <div ref={pickerColumnRef}>
+            <div onClick={handleIconClick}>
+                <AccountCircle />
+            </div>
             <div>{title}</div>
             <div>{message}</div>
-            {showModal && <Modal/>}
+            {showModal && <Modal />}
+            <div>
+                <button ref={buttonRef} onClick={save}>
+                    Save as PDF
+                </button>
+            </div>
         </div>
     );
-}
+};
 
 export default PickerColumn;
