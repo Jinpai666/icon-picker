aeacae4415aa2acb05032c17bce5016cf0769a83
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-14 15:55:19 +0200

    remove junk

--- solution/src/assets/icons.ts
+++
@@ -1,4 +0,0 @@
-export const icons = [
-     ["arrow_back", "arrow_forward", "arrow_downward", "arrow_upward"],          ["mood", "mood_bad", "sentiment_neutral", "sentiment_very_dissatisfied"],
-     ["hiking", "sports_martial_arts", "kitesurfing", "sprint"],
-]
