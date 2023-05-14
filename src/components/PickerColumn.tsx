8f47e2164a09c3937d7c7d66f9d09ffa8dc9f121
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-14 14:36:06 +0200

    fixes

--- src/components/PickerColumn.tsx
+++ src/components/PickerColumn.tsx
@@ -1,35 +1,30 @@
-import {FC, useState,} from 'react';
-import {Column} from './styles/Column.styled';
-import {Icon} from "./styles/Icon.styled";
-import PickerModal from "./PickerModal";
+import React, { FC, useState } from 'react';
+import { Column } from './styles/Column.styled';
+import { Icon } from './styles/Icon.styled';
+import PickerModal from './PickerModal';
 
-type  PickerColumnProps = {
-    active: boolean;
+type PickerColumnProps = {
+    isActive?: boolean;
     icons: string[];
-}
+};
 
-const PickerColumn: FC<PickerColumnProps> = ({icons, active}) => {
-    const handleModalClick = ()=>{
-        setShowModal(true)
-    }
+const PickerColumn: FC<PickerColumnProps> = ({ icons, isActive }) => {
+    const handleModalClick = () => {
+        setShowModal(true);
+    };
     const [showModal, setShowModal] = useState<boolean>(false);
     const [iconToPrint, setIconToPrint] = useState<string>(icons[0]);
 
     return (
-        <Column  active={active ? 1 : 0}>
-            <Icon  onClick={handleModalClick}>
-            <span  className="material-symbols-outlined">
-                    {iconToPrint}
-                </span>
+        <Column isActive={isActive}>
+            <Icon onClick={handleModalClick}>
+                <span className="material-symbols-outlined">{iconToPrint}</span>
             </Icon>
-            <input placeholder="Insert text here" type="text"/>
-            <input placeholder="Add here your additional text" type="text"/>
-            {showModal && <PickerModal
-                handleModalVisibility={setShowModal}
-                setIcon={setIconToPrint}
-                icons={icons}
-            />}
-
+            <input placeholder="Insert text here" type="text" />
+            <input placeholder="Add here your additional text" type="text" />
+            {showModal && (
+                <PickerModal handleModalVisibility={setShowModal} setIcon={setIconToPrint} icons={icons} />
+            )}
         </Column>
     );
 };
--- src/components/styles/Column.styled.ts
+++ src/components/styles/Column.styled.ts
@@ -1,13 +1,13 @@
 import styled, {css} from "styled-components";
 
-export const Column = styled.div`
+export const Column = styled.div< {isActive: boolean}>`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
   margin-bottom: 10px;
   ${(props) =>
-          props.active &&
+          props.isActive &&
           css`
       opacity: 0.3; 
     `}
