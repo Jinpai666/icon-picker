89422a1f49af45493baa63fb7eaf942ebb439541
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-14 15:16:10 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Container } from "./components/styles/Container.styled"
 import { GlobalStyles } from "./components/styles/Global";
 import Picker from "./components/Picker";
+import PickerHeader from "./components/PickerHeader";
+
 
 const App = () => {
     return (
             <Container>
+                <PickerHeader/>
+
                 <GlobalStyles/>
                 <Picker/>
             </Container>
--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
 
   body{
     position: relative;
