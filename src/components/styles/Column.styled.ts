8097caf9f16c5112d2190811b2a13302a1db14ec
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-14 14:42:03 +0200

    fix

--- src/components/styles/Column.styled.ts
+++ src/components/styles/Column.styled.ts
@@ -1,13 +1,13 @@
 import styled, {css} from "styled-components";
 
-export const Column = styled.div< {isActive: boolean}>`
+export const Column = styled.div< {active: boolean}>`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
   margin-bottom: 10px;
   ${(props) =>
-          props.isActive &&
+          props.active &&
           css`
       opacity: 0.3; 
     `}
